224a77fa6df347937af33fd2b3d5c1e8d583f8d0
S Alya Mevia <13519083@std.stei.itb.ac.id>
2022-07-28 13:12:08 +0700

    about me page

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -11,7 +11,6 @@ import hobbies from '../Content/hobbies.json';
 import lang from '../Content/lang.json';
 import skills from '../Content/skills.json';
 import work from '../Content/work.json';
-import { resolvePath } from 'react-router-dom';
 
 const RolePlaceContent = (props) => {
   let file = props.file;
@@ -41,8 +40,49 @@ const RolePlaceContent = (props) => {
     </div>
   );
 };
-const ListContent = () => {};
-const LangContent = () => {};
+const ListContent = (props) => {
+  let file = props.file;
+  return (
+    <div
+      class="gridContainter"
+      style={{
+        display: 'grid',
+        gridGap: '2rem',
+        gridAutoFlow: 'row',
+        margin: '1rem 0',
+      }}
+    >
+      {file.map((item) => {
+        return <Text>{item}</Text>;
+      })}
+    </div>
+  );
+};
+const LangContent = (props) => {
+  let file = props.file;
+  return (
+    <div
+      class="gridContainter"
+      style={{
+        display: 'grid',
+        gridGap: '2rem',
+        gridAutoFlow: 'row',
+        margin: '1rem 0',
+      }}
+    >
+      {Object.keys(file).map((key) => {
+        return (
+          <Div>
+            <Text textColor="pink">{key}</Text>
+            {file[key].map((item) => {
+              return <Text>* {item}</Text>;
+            })}
+          </Div>
+        );
+      })}
+    </div>
+  );
+};
 
 const Headline = (props) => {
   let text = props.text;
@@ -61,6 +101,21 @@ const Headline = (props) => {
 function AboutMe() {
   return (
     <>
+      <Button
+        h="3.5rem"
+        w="3.5rem"
+        pos="fixed"
+        bottom="0"
+        right="0"
+        bg="none"
+        rounded="circle"
+        border="1px solid"
+        borderColor="dark"
+        m="1rem"
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+      >
+        <Icon name="Up" size="2em" />
+      </Button>
       <Div textColor="dark" bg="lime">
         <Navbar />
         <Div
@@ -98,20 +153,20 @@ function AboutMe() {
           ></Div>
         </Div>
       </Div>
-      <Div m={{ x: '10%', y: '3em' }}>
+      <Div p={{ x: '10%', y: '3em', b: '7%' }}>
         <Div>
           <Headline text="Experience" />
           <RolePlaceContent file={work} />
           <Headline text="Education" />
           <RolePlaceContent file={edu} />
           <Headline text="Skills" />
-          {/*<ListContent file={skills} /> */}
+          <ListContent file={skills} />
           <Headline text="Professional Certification" />
           <RolePlaceContent file={cert} />
           <Headline text="Languages" />
-          {/* <LangContent file={lang} />
+          <LangContent file={lang} />
           <Headline text="Also busy with.. ♪(^_^*)" />
-          <ListContent file={hobbies} /> */}
+          <ListContent file={hobbies} />
         </Div>
       </Div>
     </>
