027d921e5fb5b3926e3e0ca336f8af48ff60fc6d
S Alya Mevia <13519083@std.stei.itb.ac.id>
2022-07-28 12:17:47 +0700

    loaded the role place json type

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
 import { Div, Text, Button, Icon } from 'atomize';
 
+import '../index.css';
 import Navbar from '../Components/Navbar.js';
-import portrait from '../Image/portrait.jpeg';
+import profile from '../Image/profile.png';
+
+import edu from '../Content/edu.json';
+import cert from '../Content/cert.json';
+import hobbies from '../Content/hobbies.json';
+import lang from '../Content/lang.json';
+import skills from '../Content/skills.json';
+import work from '../Content/work.json';
+import { resolvePath } from 'react-router-dom';
+
+const RolePlaceContent = (props) => {
+  let file = props.file;
+  return (
+    <div
+      class="gridContainter"
+      style={{
+        display: 'grid',
+        gridGap: '2rem',
+        gridAutoFlow: 'row',
+        margin: '1rem 0',
+      }}
+    >
+      {file.map((item) => {
+        return (
+          <Div>
+            <Text textColor="pink">{item.role}</Text>
+            <a href={item.placelink} target="_blank" rel="noreferrer">
+              <Text textColor="dark" textDecor="underline">
+                @ {item.place}
+              </Text>
+            </a>
+            <Text>{item.duration}</Text>
+          </Div>
+        );
+      })}
+    </div>
+  );
+};
+const ListContent = () => {};
+const LangContent = () => {};
 
 const Headline = (props) => {
   let text = props.text;
@@ -11,6 +51,7 @@ const Headline = (props) => {
       fontFamily="heading"
       textSize={{ xs: '24px', md: '36x' }}
       textColor="teal"
+      m={{ t: '1.5em' }}
     >
       {text}
     </Text>
@@ -20,54 +61,57 @@ const Headline = (props) => {
 function AboutMe() {
   return (
     <>
-      <Div textColor="dark" bg="lime" p={{ b: '3em' }}>
+      <Div textColor="dark" bg="lime">
         <Navbar />
-        <Div d="flex" flexDir="row">
-          <Div
-            d="flex"
-            flexDir="column"
-            p={{ x: '10%' }}
-            justify="center"
-            h="85%"
-            m="0px"
-          >
-            <Text
-              fontFamily="display"
-              textSize={{ xs: '36px', md: '48px' }}
-              maxW="11em"
-            >
+        <Div
+          d="flex"
+          flexDir={{ xs: 'column', sm: 'column', md: 'row' }}
+          p={{ x: '10%', b: '1em' }}
+          align="center"
+          justify="space-between"
+        >
+          <Div d="flex" flexDir="column" justify="center" h="85%" m="0px">
+            <Text fontFamily="display" textSize="48px">
               ⁕My name is <span style={{ color: '#FF6B6C' }}>S</span>haffira{' '}
               <span style={{ color: '#FF6B6C' }}>Alya Mevia</span>
             </Text>
             <Text
-              textSize={{ sm: '16px', md: '18px' }}
+              textSize="18px"
               textWeight="600"
-              m={{ y: { xs: '3rem', md: '2rem' } }}
-              maxW="35em"
+              m={{ y: '2rem', b: { xs: '3rem', sm: '0px' } }}
+              maxW="45em"
             >
               I always have the passion to study languages. So far I have
-              learned Bahasa Indonesia (my mother language), English, Japanese,
-              Korean, Deutsch, Python, JavaScript, C, C++, and... oh! It’s a
-              different type of language? Whoops~
+              learned Bahasa Indonesia, English, Japanese, Korean, Deutsch,
+              Python, SQL, JavaScript, C, C++, and... oh! It's a different type
+              of language? Whoops~
             </Text>
           </Div>
           <Div
             style={{
-              backgroundImage: `url(${portrait})`,
+              backgroundImage: `url(${profile})`,
               backgroundRepeat: 'no-repeat',
             }}
-            bgPos="100% 20%"
-            bgSize="120%"
-            minW="20vw"
-            minH="15em"
-            rounded={{ t: 'circle' }}
+            bgSize="100%"
+            minW="20em"
+            minH="20em"
           ></Div>
         </Div>
       </Div>
       <Div m={{ x: '10%', y: '3em' }}>
         <Div>
           <Headline text="Experience" />
-          <Div></Div>
+          <RolePlaceContent file={work} />
+          <Headline text="Education" />
+          <RolePlaceContent file={edu} />
+          <Headline text="Skills" />
+          {/*<ListContent file={skills} /> */}
+          <Headline text="Professional Certification" />
+          <RolePlaceContent file={cert} />
+          <Headline text="Languages" />
+          {/* <LangContent file={lang} />
+          <Headline text="Also busy with.. ♪(^_^*)" />
+          <ListContent file={hobbies} /> */}
         </Div>
       </Div>
     </>
